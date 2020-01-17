c5da5e9367c939b37a1ec4a552841ff0c0d5aad3
Ciro Liberato Carmin <zziro.123@gmail.com>
2020-01-17 15:56:42 -0500

    CI/CD

---
+++ demo-ci/src/main/java/com/zziroacademy/democi/DemoCiApplication.java
@@ -0,0 +1,13 @@
+package com.zziroacademy.democi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCiApplication.class, args);
+	}
+
+}
---
+++ demo-ci/src/test/java/com/zziroacademy/democi/DemoCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zziroacademy.democi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
